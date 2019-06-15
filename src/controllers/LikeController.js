5e43f8e61811ef2a4c7d2d4bf013dedc9cfd32ad
unknown <Leandro Severino@POAW10N09344.resource.com.br>
2019-06-15 09:07:58 -0300

    Add eslint

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,16 +1,15 @@
+/* eslint-disable linebreak-style */
 const Post = require('../models/Post');
 
 module.exports = {
-    async store(req, res) {
-      
-      const post = await Post.findById(req.params.id);
+  async store(req, res) {
+    const post = await Post.findById(req.params.id);
 
-      post.likes += 1;
-      await post.save();
+    post.likes += 1;
+    await post.save();
 
-      req.io.emit('like', post);
-      
-      return res.json(post);
-    }
+    req.io.emit('like', post);
 
-}
+    return res.json(post);
+  },
+};
